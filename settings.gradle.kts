7600d387bf0564e7674f32dbe4a19439d9a8cace
Mithat Sinan Sarı <36641492+mitsinsar@users.noreply.github.com>
2024-11-04 22:11:06 +0300

    PERA-1048 :: Account refactor data layer  (#22)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,21 @@
-include(":app")
 rootProject.name = "algorand-android"
+include(":app")
+include(":wallet-sdk")
+include(":test-utils")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
+        maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+    }
+}
